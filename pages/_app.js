b465494952a16e6938f3eef71a9eff29eb077dbd
cst016 <cst016@vcc.ca>
2021-12-13 13:09:24 -0800

    initial commit + code

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
+import React from 'react'
+import Head from 'next/head'
+import '../src/styles/globals.css'
+import '../src/styles/header.css'
+import '@emotion/react'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
-}
 
+}
 export default MyApp
---
+++ src/components/firstpage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from '../styles/Home.module.css'
+//import Box from '@mui/material/Box'
+
+export default function Home() {
+
+  return (
+
+      <div className={styles.container}>
+        <hi>Test</hi>
+      </div>
+
+  )
+}
+
+
